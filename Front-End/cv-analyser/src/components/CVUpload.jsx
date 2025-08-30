07daddcfbf0546982807223d5441a32fee9e085d
Jaden Toon <jaden.toon@gmail.com>
2025-08-30 17:11:03 +0100

    First iteration of CVUpload.jsx

---
+++ Front-End/cv-analyser/src/components/CVUpload.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const CVUpload = () => {
+    const [file, setFile] = useState(null);
+
+
+    return (
+        <div>
+            <input 
+            type="file" 
+            onChange={e => setFile(e.target.files?.[0] || null)}/>
+            <button className='px-3 py-1 bg-blue-500 text-white rounded'>Upload CV</button>
+        </div>
+    )
+}
+
+export default CVUpload
---
+++ Front-End/cv-analyser/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CVUpload from './CVUpload'
+
+const Home = () => {
+  return (
+    <div className='p-10'>
+        <h1 className='font-bold text-3xl mb-4'>CV Analyser</h1>
+        <CVUpload />
+    </div>
+  )
+}
+
+export default Home
