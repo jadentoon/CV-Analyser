1fca04613b3fd270261ef939be7bbfe868f4a231
Jaden Toon <jaden.toon@gmail.com>
2025-09-13 19:01:57 +0100

    Added removing file box in the file upload section

--- Front-End/cv-analyser/src/components/CVUpload.jsx
+++ Front-End/cv-analyser/src/components/CVUpload.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from "axios";
+import {Trash2,NotepadText} from "lucide-react";
 
 const CVUpload = () => {
     const [file, setFile] = useState(null);
@@ -8,6 +9,11 @@ const CVUpload = () => {
 
     const handleFileChange = (e) => {
         setFile(e.target.files?.[0] || null);
+        setMessage("");
+    }
+
+    const handleRemoveFile = () => {
+        setFile(null);
         setMessage("");
     }
 
@@ -45,7 +51,7 @@ const CVUpload = () => {
         <div className='flex flex-col gap-4'>
             <label className='flex flex-col items-center justify-center w-full h-40 border-2 border-dashed border-gray-300 rounded-lg cursor-pointer hover:border-blue-400 transition bg-gray-50'>
                 <p className='text-gray-500'>
-                    {file ? `Selected file: ${file.name}` : "Drag & drop a PDF here of click to select"}
+                    {file ? "File ready to upload" : "Drag & drop a PDF here of click to select"}
                 </p>
                 <input
                     type="file"
@@ -55,6 +61,28 @@ const CVUpload = () => {
                 />
             </label>
 
+            {file && (
+                <div className='flex items-center justify-between bg-blue-50 p-3 rounded-lg'>
+                    <div className='flex items-center gap-2'>
+                        <span className='bg-blue-200 w-6 h-6 flex items-center justify-center rounded'>
+                            <NotepadText className="w-4 h-4"/>
+                        </span>
+                        <div>
+                            <p className='text-gray-800 font-medium'>{file.name}</p>
+                            <p className='text-gray-500 text-sm'>{(file.size / 1024 / 1024).toFixed(2)} MB</p>
+                        </div>
+                    </div>
+                    <button
+                        onClick={handleRemoveFile}
+                        className='p-2 rounded hover:bg-red-100 transition cursor-pointer'
+                        aria-label="Remove File"
+                    >
+                        <Trash2 className="w-5 h-5 text-gray-500 hover:text-red-500 transition-colors"/>
+                    </button>
+
+                </div>
+            )}
+
             <button
                 onClick={handleUpload}
                 disabled={isUploading}
@@ -63,7 +91,7 @@ const CVUpload = () => {
             >
                 {isUploading ? "Uploading..." : "Upload CV"}
             </button>
-            
+
             {message && (
                 <p className={`text-sm ${message.startsWith("✔️") ? "text-green-600" : "text-red-600"} >`}>
                     {message}
